8fa01b0c3f4da99c8df59f2925c17eff43efc673
35812 <yicijike@126.com>
2017-06-20 14:11:57 +0800

    revison method of main

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,10 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
         System.out.println("fist commit!");
+
+        System.out.println("second commit!");
+
     }
 }
